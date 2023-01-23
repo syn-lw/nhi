b911ff0b6ff4a6a32ffc0ef7fb8c4b0d4a3c5689
syn-lw <louis.wolfers@synacktiv.com>
2023-01-23 15:52:37 +0000

    feat(cli): add output search functionality

--- cli/cli.py
+++ cli/cli.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-import sqlite3
+import argparse
 import datetime
+import sqlite3
 import subprocess
 from iterfzf import iterfzf
 
@@ -8,16 +9,19 @@ def get_tables(conn):
     tables_query = "SELECT name FROM sqlite_master WHERE type='table' AND name!='meta'"
     cursor = conn.cursor()
     cursor.execute(tables_query)
-    #return cursor.fetchall()
     return [result["name"].decode() for result in cursor.fetchall()]
 
-def get_logs(conn, tables):
+def get_logs(conn, tables, output_filter=None):
     assert all(table.isnumeric() for table in tables), "table name not numeric"
     list_of_logs = []
     for table in tables:
-        logs_query = f'SELECT * FROM "{table}"'
         cursor = conn.cursor()
-        cursor.execute(logs_query)
+        if output_filter:
+            logs_query = f'SELECT * FROM "{table}" WHERE output LIKE ?'
+            cursor.execute(logs_query, [f"%{output_filter}%"])
+        else:
+            logs_query = f'SELECT * FROM "{table}"'
+            cursor.execute(logs_query)
         list_of_logs.extend(cursor.fetchall())
     return list(filter(lambda x: x["command"] != None, list_of_logs))
 
@@ -45,11 +49,24 @@ def main():
     conn.text_factory = bytes
     conn.row_factory = sqlite3.Row
 
+    parser = argparse.ArgumentParser(description='Search and print logs.')
+    parser.add_argument('--output', help='Search for a string in the command\'s output.')
+    parser.add_argument('--debug', help='Print SQLite queries.', action='store_true')
+    args = parser.parse_args()
+
+    if args.debug:
+        conn.set_trace_callback(print)
+
     tables = get_tables(conn)
-    logs = get_logs(conn, tables)
+    if args.output:
+        logs = get_logs(conn, tables, args.output)
+    else:
+        logs = get_logs(conn, tables)
+
     # remove CRLF for fzf
     commands = [(index, value["command"].decode().replace("\n", " ").replace("\r", " ")) for (index, value) in zip(range(len(logs)), logs)]
     commands = [f"{index} {command}" for (index, command) in commands]
+
     logs_selected = iterfzf(commands[::-1], multi=True)
     if logs_selected != None:
         logs_index = map(int, [selection.split(" ")[0] for selection in logs_selected])
